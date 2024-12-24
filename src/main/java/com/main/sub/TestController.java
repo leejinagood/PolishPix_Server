8d495fde3804d04020e3efb23905234827dd0e19
이진아 <mnb2098@naver.com>
2024-12-24 13:05:02 +0900

    환경 변수 세

--- src/main/java/com/main/sub/TestController.java
+++ src/main/java/com/main/sub/TestController.java
@@ -1,13 +1,15 @@
-//package com.main.sub;
-//
-//import org.springframework.*;
-//
-//@Controller
-//public class TestController{
-//	@GetMapping("/test")
-//	@ResponseBody
-//	public String Test() {
-//		return "Hello World!!";
-//	}
-//}
+package com.main.sub;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class TestController{
+	@GetMapping("/test")
+	@ResponseBody
+	public String Test() {
+		return "Hello World!!";
+	}
+}
