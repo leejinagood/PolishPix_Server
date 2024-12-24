32de791843aa8428360a00c23dd6d1acd473a353
이진아 <mnb2098@naver.com>
2024-12-24 12:49:51 +0900

    test2

--- src/main/java/com/main/sub/TestController.java
+++ src/main/java/com/main/sub/TestController.java
@@ -1,12 +1,12 @@
-package com.main.sub;
-
-import org.springframework.*;
-
-@Controller
-public class TestController{
-	@GetMapping("/test")
-	@ResponseBody
-	public String Test() {
-		return "Hello World!!";
-	}
-}
+//package com.main.sub;
+//
+//import org.springframework.*;
+//
+//@Controller
+//public class TestController{
+//	@GetMapping("/test")
+//	@ResponseBody
+//	public String Test() {
+//		return "Hello World!!";
+//	}
+//}
